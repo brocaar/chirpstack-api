a2da999bcc577c175f3da58e0f8770109d970702
Kiara Navarro <sophiekovalevsky@fedoraproject.org>
2020-05-02 21:24:16 -0300

    Add network_server_name to DeviceProfileListItem
    Signed-off-by: Kiara Navarro <sophiekovalevsky@fedoraproject.org>

--- python/src/setup.py
+++ python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "3.3.2",
+    version = "3.4.1",
     url='https://github.com/brocaar/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
