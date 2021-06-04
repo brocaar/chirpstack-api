cebb8a8e488498a1559e14f7276a4658683fa77e
Orne Brocaar <info@brocaar.com>
2021-06-04 09:25:36 +0100

    Add request_fragmentation_session_status field to FUOTA deployment.

--- python/src/setup.py
+++ python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "3.9.10",
+    version = "3.9.11",
     url='https://github.com/brocaar/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
